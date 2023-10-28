d520ee002b5250d6449f30b64d85b3943fe84f29
MustafaCebeci <ytr1mustafa@gmail.com>
2023-10-28 14:09:01 +0300

    sw

---
+++ public/frontend/js/theme.js
@@ -0,0 +1,14 @@
+let yol1 ="../css/gunduz/index.css"
+let yol2 ="../css/gece/index.css"
+let css = document.querySelector("#theme")
+
+let ico = document.getElementById("swifty")
+
+ico.addEventListener("click",()=>{
+    ico.classList.toggle("swi")
+    console.log(ico)
+    console.log(css)
+    console.log(sayi)
+})
+
+
